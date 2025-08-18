749b02c55e41fab13ebd52fe140de513cae07328
laith_waseem <hayder.hayder2010@gmail.com>
2025-08-18 18:05:54 +0300

    1.1

---
+++ product.js
@@ -0,0 +1,83 @@
+const img = document.getElementById('mainProductCover');
+
+if (window.location.search === "?adidas") {
+    img.setAttribute("src", "yy phone IMG/adidas.png")
+} else if (window.location.search === "?adidascolored") {
+    img.setAttribute("src", "yy phone IMG/adidascolored.png")
+} else if (window.location.search === "?bentley") {
+    img.setAttribute("src", "yy phone IMG/bentley.png")
+} else if (window.location.search === "?bk1") {
+    img.setAttribute("src", "yy phone IMG/bk1.jpg")
+} else if (window.location.search === "?bk2") {
+    img.setAttribute("src", "yy phone IMG/bk2.jpg")
+} else if (window.location.search === "?bmw") {
+    img.setAttribute("src", "yy phone IMG/bmw.png")
+} else if (window.location.search === "?cars") {
+    img.setAttribute("src", "yy phone IMG/cars.jpg")
+} else if (window.location.search === "?CF1") {
+    img.setAttribute("src", "yy phone IMG/CF1.jpg")
+} else if (window.location.search === "?CF2") {
+    img.setAttribute("src", "yy phone IMG/CF2.jpg")
+} else if (window.location.search === "?combo1") {
+    img.setAttribute("src", "yy phone IMG/combo1.jpg")
+} else if (window.location.search === "?combo2") {
+    img.setAttribute("src", "yy phone IMG/combo2.jpg")
+} else if (window.location.search === "?dior") {
+    img.setAttribute("src", "yy phone IMG/dior.jpg")
+} else if (window.location.search === "?eyes") {
+    img.setAttribute("src", "yy phone IMG/eyes.jpg")
+} else if (window.location.search === "?eyes2") {
+    img.setAttribute("src", "yy phone IMG/eyes2.jpg")
+} else if (window.location.search === "?eyesblack") {
+    img.setAttribute("src", "yy phone IMG/eyesblack.jpg")
+} else if (window.location.search === "?face") {
+    img.setAttribute("src", "yy phone IMG/face.png")
+} else if (window.location.search === "?flowers") {
+    img.setAttribute("src", "yy phone IMG/flowers.png")
+} else if (window.location.search === "?flowers1") {
+    img.setAttribute("src", "yy phone IMG/flowers1.jpg")
+} else if (window.location.search === "?flowers2") {
+    img.setAttribute("src", "yy phone IMG/flowers2.jpg")
+} else if (window.location.search === "?flowers3") {
+    img.setAttribute("src", "yy phone IMG/flowers3.jpg")
+} else if (window.location.search === "?flowers4") {
+    img.setAttribute("src", "yy phone IMG/flowers4.jpg")
+} else if (window.location.search === "?knit") {
+    img.setAttribute("src", "yy phone IMG/knit.jpg")
+} else if (window.location.search === "?knit1") {
+    img.setAttribute("src", "yy phone IMG/knit1.jpg")
+} else if (window.location.search === "?knit2") {
+    img.setAttribute("src", "yy phone IMG/knit2.jpg")
+} else if (window.location.search === "?knit3") {
+    img.setAttribute("src", "yy phone IMG/knit3.jpg")
+} else if (window.location.search === "?knit4") {
+    img.setAttribute("src", "yy phone IMG/knit4.jpg")
+} else if (window.location.search === "?lacoste") {
+    img.setAttribute("src", "yy phone IMG/lacoste.jpg")
+} else if (window.location.search === "?lacoste1") {
+    img.setAttribute("src", "yy phone IMG/lacoste1.jpg")
+} else if (window.location.search === "?lacoste2") {
+    img.setAttribute("src", "yy phone IMG/lacoste2.jpg")
+} else if (window.location.search === "?lacoste3") {
+    img.setAttribute("src", "yy phone IMG/lacoste3.jpg")
+} else if (window.location.search === "?magsafe") {
+    img.setAttribute("src", "yy phone IMG/magsafe.jpg")
+} else if (window.location.search === "?medalions") {
+    img.setAttribute("src", "yy phone IMG/medalions.jpg")
+} else if (window.location.search === "?nba") {
+    img.setAttribute("src", "yy phone IMG/nba.jpg")
+} else if (window.location.search === "?silicone") {
+    img.setAttribute("src", "yy phone IMG/silicone.jpg")
+} else if (window.location.search === "?silver") {
+    img.setAttribute("src", "yy phone IMG/silver.png")
+} else if (window.location.search === "?silver1") {
+    img.setAttribute("src", "yy phone IMG/silver1.jpg")
+} else if (window.location.search === "?silver2") {
+    img.setAttribute("src", "yy phone IMG/silver2.jpg")
+} else if (window.location.search === "?snake") {
+    img.setAttribute("src", "yy phone IMG/snake.jpg")
+} else if (window.location.search === "?tesla") {
+    img.setAttribute("src", "yy phone IMG/tesla.jpg")
+} else if (window.location.search === "?versace") {
+    img.setAttribute("src", "yy phone IMG/versace.jpg")
+}
